2f2aeb017054db8b776c6c3f583a41ebb4eb1c79
Anuvab123 <ghoshanuvab2000@gmail.com>
2023-04-13 23:39:48 +0530

    Initial commit

---
+++ Zippopatam/src/test/java/com/geekster/Zippopatam/ZippopatamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.Zippopatam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZippopatamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
